5083c9996cb5c3b00eb4ad9999dead2bc15de608
kunxin-chor <chorkunxin@yahoo.com>
2026-04-08 06:16:04 +0000

    Added CRUD example

--- 04-list-processing/data.js
+++ 04-list-processing/data.js
@@ -3,20 +3,20 @@
 // in the html
 let todos = [
     {
-        "id":  1,
-        "description":"Wash the car",
+        "id": 1,
+        "description": "Wash the car",
         "done": false,
         "date": "2026-04-07"
     },
-        {
-        "id":  2,
-        "description":"Clean the room",
+    {
+        "id": 2,
+        "description": "Clean the room",
         "done": false,
         "date": "2026-04-08"
     },
-        {
-        "id":  3,
-        "description":"Go to gym",
+    {
+        "id": 3,
+        "description": "Go to gym",
         "done": false,
         "date": "2026-04-09"
     }
@@ -44,4 +44,26 @@ function deleteTodo(todos, idToDelete) {
     }
 
     todos.splice(indexToDelete, 1);
+}
+
+// when we do the update operation, we assume the user is changing
+// all the details of the record (except the id)
+function updateTodo(todos, idToUpdate, newDescription, newDone, newDate) {
+    let indexToUpdate = -1;
+
+    // given the id of the todo that we are changing, what is its index?
+    // arrays have a findIndex function
+    // it takes one parameter, which is a function
+    // all items in the array will be passed to the function one by one
+    // if the function returns true, then we will take the index of that item
+    indexToUpdate = todos.findIndex(task => task.id == idToUpdate);
+    todos[indexToUpdate] = {
+        id: idToUpdate,
+        description: newDescription,
+        done: newDone,
+        date: newDate
+    }
+
+
+
 }
--- 04-list-processing/script.js
+++ 04-list-processing/script.js
@@ -1,8 +1,8 @@
 function displayTodos(todos) {
     let todoList = document.querySelector("#list");
-    todoList.innerHTML ="";
+    todoList.innerHTML = "";
     for (let t of todos) {
-     
+
         // create a new html element using JavaScript
 
         // create a new <li>
@@ -12,20 +12,35 @@ function displayTodos(todos) {
 
         // newListItem.innerHTML = "t.description"
         newListItem.innerHTML = `${t.description} (done:${t.done}, date:${t.date}) 
-                                 <button class="btn-delete">🗑️</button>`
+                                 <a class="btn-delete" href="#">🗑️</a>
+                                 <a class="btn-update" href="#">✏️</a>
+                                 
+                                 `
 
         // querySelector and querySelectorAll can be called on any HTML element
         const deleteButton = newListItem.querySelector(".btn-delete");
-        deleteButton.addEventListener("click", function(){
+        deleteButton.addEventListener("click", function (event) {
+            event.preventDefault();
             deleteTodo(todos, t.id);
+            displayTodos(todos);
+        })
+
+        const updateButton = newListItem.querySelector(".btn-update");
+        updateButton.addEventListener("click", function (event) {
+            event.preventDefault();
+            // the second parameter of the prompt function will be the default
+            const newDescription = prompt("Enter the new description", t.description);
+            const newDone = prompt("Enter y if it is done") == "y";
+            const newDate = prompt("Enter the date", t.date);
+            updateTodo(todos, t.id, newDescription, newDone, newDate);
             displayTodos(todos);
         })
 
     }
 }
 
-let form =document.querySelector("#add-new-todo");
-form.addEventListener("submit", function(event){
+let form = document.querySelector("#add-new-todo");
+form.addEventListener("submit", function (event) {
     // prevent the browser to submit the form
     event.preventDefault();
 
