a909623ed047a4cf93d9984b6b2da7069484e0c8
kunxin-chor <chorkunxin@yahoo.com>
2026-04-09 06:38:27 +0000

    added add and display movie

---
+++ 11-movies/script.js
@@ -0,0 +1,39 @@
+function displayAllMovies(movies) {
+    const moviesList = document.querySelector("#movies");
+    
+    // remove all existing movies first
+    moviesList.innerHTML = "";
+    for (let eachMovie of movies) {
+        // 1. Create an element to store the card
+        const movieCardEl = document.createElement("div");
+        moviesList.appendChild(movieCardEl);
+
+        // <div class="card" style="width: 18rem"></div>
+        movieCardEl.className = "card mt-1 mb-1";
+        movieCardEl.style.width = "18rem";
+
+        movieCardEl.innerHTML = `
+         <div class="card-body">
+          <h5 class="card-title">${eachMovie.title}</h5>
+          <p class="card-text">Genre: ${eachMovie.genre}</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+        `
+    }
+}
+
+displayAllMovies(movies);
+
+document.querySelector("#addMovieBtn").addEventListener("click", function(){
+    // get the inputs from the form
+    const title = document.querySelector("#title").value;
+    const genre = document.querySelector("#genre").value;
+
+    // process input: create a new movie object
+    // and add it to the movie list
+    addMovie(movies, title, genre);
+
+    // refresh the movie display
+    displayAllMovies(movies);
+
+}) // end add event listener
